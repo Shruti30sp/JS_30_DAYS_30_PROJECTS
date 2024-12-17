01f9e7ceae18a1f5c9045fa33a2da171cd2d9f13
priynkapatil <priyankapatilp22@gmail.com>
2024-12-17 23:25:45 +0530

    Third project on TODO

---
+++ Day03/todo.js
@@ -0,0 +1,26 @@
+const inputField = document.getElementById("input");
+const listContainer = document.getElementById("tasks");
+function addInput() {
+  if (inputField.value === "") {
+    alert("Please write something to add!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputField.value = "";
+}
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.ClassList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
